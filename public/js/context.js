0b4220836099a260fc74a3d2616fbe1e703811dc
tfrazzitta <tfrazzitta@yahoo.com>
2020-07-24 18:00:05 -0400

    l

--- public/js/context.js
+++ public/js/context.js
@@ -7,7 +7,7 @@ var context = {
 				item:"LFC Scarf",
 				price:"24.99",
 				text: "Officially licensed UK Import scarf. Features team colors and crest. Full size - Knitted Approx 5ft long. Perfect for any Liverpool fan! As sold in the club shop at Anfield",
-				src: "https://m.media-amazon.com/images/I/61gL7DYAEwL._AC_SS350_.jpg"
+				src: "/images/scarf.jpg"
 			},
 
 			{
@@ -15,7 +15,7 @@ var context = {
 				item:"LFC t-shirt",
 				price:"19.99",
 				text: "New Official Liverpool FC heather grey t-shirt. 90% cotton / 10% polyester. Slim fit construction. Order a size up if you prefer a looser fit.",
-				src: "https://images-na.ssl-images-amazon.com/images/I/61iOoN80HdL._AC_SX425_.jpg"
+				src: "/images/TeeFront.jpg"
 			},
 
 			{
@@ -23,7 +23,8 @@ var context = {
 				item:"LFC Hoodie",
 				price:"49.99",
 				text: "A classic. Bold red print on a black blend pullover hoodie. , 7.8-ounce, 50/50 cotton/poly fleece , Air jet yarn for a soft, pill-resistant finish , ...",
-				src: "https://www.anfieldshop.com/images/products/lic%20hoodie%20liverbird.png"
+				src: "/images/hoodie-back.jpg",
+				alt: "/images/hoodie-front.jpg"
 			}
 
 
