50b62284a9a7f6dfa79734038a0a84acc155ecf6
tfrazzitta <tfrazzitta@yahoo.com>
2020-08-01 01:58:58 -0400

    l

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.port || 3000;
 var nodeMailer = require('nodemailer');
 var token=require('./node_modules/encodeurl/token.js')
 var credentials=require(token)
