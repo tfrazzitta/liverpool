d74a4cb00dba754da6a74a5bdae8a1fa54fee857
tfrazzitta <tfrazzitta@yahoo.com>
2020-12-03 09:39:10 -0500

    l

--- public/js/context.js
+++ public/js/context.js
@@ -8,16 +8,24 @@ var context = {
 				price:"$ 15.00",
 				text: "Officially licensed UK Import scarf. Features team colors and crest. Full size - Knitted Approx 5ft long. Perfect for any Liverpool fan! As sold in the club shop at Anfield",
 				src: "/images/scarf.jpg",
+				//FREE SHIPPING
+				//addToCart:'<form class="text-center pad" target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="NLVKZYNR28C7G"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>',
+				//buyNow:'<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="P3D89FGPACM6E"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>'				//REGULAR
+				
+				//regular
 				buyNow:'<form class="text-center pad" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="MMAKDFY3M4UC6"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>',
-
 				addToCart:'<form class="text-center pad" target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="NLVKZYNR28C7G"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>'
+
+
+
+
 
 			},
 			{
 				id:1,
-				item:"LFC t-shirt",
-				price:"$ 20.00",
-				text: "New Official Liverpool FC heather grey t-shirt. 90% cotton / 10% polyester. Slim fit construction. Order a size up if you prefer a looser fit.",
+				item:"LFC t-shirt **New Arrival**",
+				price:"$ 25.00",
+				text: "New Official Liverpool FC crimson red t-shirt. 90% cotton / 10% polyester. Slim fit construction.",
 				src: "/images/TeeBack1.jpg",
 				alt: "/images/TeeFront.jpg",
 				buyNow:'<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="2FYWTBUHCWEUJ"><table><tr><td><input type="hidden" name="on0" value="LFC t-shirt">LFC t-shirt</td></tr><tr><td><select name="os0">	<option value="XL">XL </option>	<option value="2XL">2XL </option></select> </td></tr></table><input class="inp-pad" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>',
@@ -28,6 +36,8 @@ var context = {
 				//addToCart:'<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="96EHSEBRD7V6A"><table><tr><td><input type="hidden" name="on0" value="LFC t-shirt">LFC t-shirt</td></tr><tr><td><select name="os0">	<option value="Small">Small </option>	<option value="Medium">Medium </option>	<option value="Large">Large </option>	<option value="XL">XL </option>	<option value="2XL">2XL </option>	<option value="3XL">3XL </option></select> </td></tr></table><input class="inp-pad" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>'
 
 				},
+
+
 
 			{ 
 				id:2,
@@ -45,6 +55,24 @@ var context = {
 				//addToCart:'<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="PGVSXDERQ42S8"><table><tr><td><input type="hidden" name="on0" value="LFC Hoodie">LFC Hoodie</td></tr><tr><td><select name="os0">	<option value="Small">Small </option>	<option value="Medium">Medium </option>	<option value="Large">Large </option>	<option value="XL">XL </option>	<option value="2XL">2XL </option>	<option value="3XL">3XL </option></select> </td></tr></table><input class="inp-pad" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>'
 			
 
-			}
+			},
+
+
+
+				{
+				id:3,
+				item:"LFC t-shirt",
+				price:"$ 20.00",
+				text: "New Official Liverpool FC heather grey t-shirt. 90% cotton / 10% polyester. Slim fit construction.",
+				src: "/images/TeeFront-grey.jpg",
+				alt: "/images/TeeBack1-grey.jpg",
+				buyNow:'<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="CPWYVC66HAXQG"><table><tr><td><input type="hidden" name="on0" value="LFC t-shirt">LFC t-shirt</td></tr><tr><td><select name="os0">	<option value="XL">XL </option>	<option value="2XL">2XL </option></select> </td></tr></table><input class="inp-pad" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>',
+				addToCart:'<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="WCETSFJLGJE2J"><table><tr><td><input type="hidden" name="on0" value="LFC t-shirt">LFC t-shirt</td></tr><tr><td><select name="os0">	<option value="XL">XL </option>	<option value="2XL">2XL </option>	</select> </td></tr></table><input class="inp-pad" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>'
+				//REGULAR
+				//buyNow:'<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="2FYWTBUHCWEUJ"><table><tr><td><input type="hidden" name="on0" value="LFC t-shirt">LFC t-shirt</td></tr><tr><td><select name="os0">	<option value="Small">Small </option>	<option value="Medium">Medium </option>	<option value="Large">Large </option>	<option value="XL">XL </option>	<option value="2XL">2XL </option>	<option value="3XL">3XL </option></select> </td></tr></table><input class="inp-pad" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>',
+				//addToCart:'<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="96EHSEBRD7V6A"><table><tr><td><input type="hidden" name="on0" value="LFC t-shirt">LFC t-shirt</td></tr><tr><td><select name="os0">	<option value="Small">Small </option>	<option value="Medium">Medium </option>	<option value="Large">Large </option>	<option value="XL">XL </option>	<option value="2XL">2XL </option>	<option value="3XL">3XL </option></select> </td></tr></table><input class="inp-pad" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>'
+
+				},
+
 	]
 }
